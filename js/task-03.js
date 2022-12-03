f5f81f228a388b1235a082721b9712785d1c613a
AndreyLisTarasov <109020493+AndreyLisTarasov@users.noreply.github.com>
2022-12-03 12:19:35 +0200

    Summary(required)

--- js/task-03.js
+++ js/task-03.js
@@ -24,10 +24,10 @@ const images = [
   },
 ];
 
-const galleryRef = document.querySelector(`.gallery`);
+const galleryEl = document.querySelector(`.gallery`);
 
 const createGalleryMarkup = ({ url, alt }) => `<li><img src=${url} alt=${alt} class=pictures></li>`;
-const createGallery = images.map(createGalleryMarkup).join(``);
-galleryRef.insertAdjacentHTML("afterbegin", createGallery);
+const createGallery = images.map(createGalleryMarkup);
+galleryEl.insertAdjacentHTML("beforeEnd", createGallery);
 
 
